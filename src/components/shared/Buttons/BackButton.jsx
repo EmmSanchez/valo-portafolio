6bbaee3a9b104726eb015c59695be0dab26018b6
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-18 17:01:44 -0600

    Botón con estilos modificable

--- src/components/shared/Buttons/BackButton.jsx
+++ src/components/shared/Buttons/BackButton.jsx
@@ -5,7 +5,10 @@ import { AppContext } from "@/context/AppContext";
 import { useLocation } from "react-router";
 import { SPLASH_KEYS } from "@/components/shared/SplashScreen/const";
 
-export default function BackButton({ to = "" }) {
+export default function BackButton({
+  to = "",
+  className = "bg-valo hover:bg-white text-white hover:text-valo",
+}) {
   const navigate = useNavigate();
   const { pathname } = useLocation();
   const { toggleSplashScreen } = useContext(AppContext);
@@ -26,20 +29,15 @@ export default function BackButton({ to = "" }) {
   return (
     <button
       onClick={() => handleBack()}
-      className="group flex justify-between items-center w-[clamp(180px,16.2vw,311px)] h-[clamp(38px,3.65vw,70px)] px-[29px] 2xl:px-11.25 bg-valo hover:bg-white hover:cursor-pointer -translate-y-0.5"
+      className={`group flex justify-between items-center w-[clamp(180px,16.2vw,311px)] h-[clamp(38px,3.65vw,70px)] px-[29px] 2xl:px-11.25 hover:cursor-pointer -translate-y-0.5 ${className}`}
     >
-      <p className="text-paragraph-button font-semibold text-white group-hover:text-valo group-hover:font-bold">
+      <p className="text-paragraph-button font-semibold group-hover:font-bold">
         Regresar
       </p>
 
       <span className="relative size-[clamp(16px,1.35vw,26px)]">
-        {/* Inactive */}
-        <span className="absolute inset-0 group-hover:hidden">
-          <ReturnIcon isActive={false} />
-        </span>
-        {/* Hover / Active */}
-        <span className="absolute inset-0 hidden group-hover:block">
-          <ReturnIcon isActive />
+        <span className="absolute inset-0">
+          <ReturnIcon />
         </span>
       </span>
     </button>
