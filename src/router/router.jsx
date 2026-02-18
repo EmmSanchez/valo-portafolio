19cfa54503c76f7c2df3bd4b1c4670768d6a3ee3
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-18 13:18:40 -0600

    Importación de rutas del proyecto "Valo Park Santa Catarina"

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router";
-import ValoPortafolioRoutes from "@/apps/valoPortafolio/pages/routes";
+import ValoPortafolioRoutes from "@/apps/valoPortafolio/routes";
+import ValoParkSantaCatarinaRoutes from "@/apps/valoParkSantaCatarina/routes";
 
 export default function Router() {
   return (
     <BrowserRouter basename="/">
       <ValoPortafolioRoutes />
+      <ValoParkSantaCatarinaRoutes />
     </BrowserRouter>
   );
 }
