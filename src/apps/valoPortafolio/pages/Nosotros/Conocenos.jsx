2aab34c207cf29e16440f27c26d9c45a6e3a2888
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-19 02:02:31 -0600

    Reemplazo de carrusel anterior por carrusel reutilizable

--- src/apps/valoPortafolio/pages/Nosotros/Conocenos.jsx
+++ src/apps/valoPortafolio/pages/Nosotros/Conocenos.jsx
@@ -1,12 +1,15 @@
-import { ConoceValoCarousel } from "@/apps/valoPortafolio/components/Carousel/ConoceValoCarousel";
 import BackButton from "@/components/shared/Buttons/BackButton";
+import { Carrousel } from "@/components/shared/Carrousel/Carrousel";
 import React from "react";
+import Slide1 from "../../components/Carousel/Slides/Slide1";
+import Slide2 from "../../components/Carousel/Slides/Slide2";
+import Slide3 from "../../components/Carousel/Slides/Slide3";
 
 export default function Conocenos() {
   return (
     <div className="w-dvw h-dvh flex flex-col default-padding">
       <div className="flex flex-1">
-        <ConoceValoCarousel />
+        <Carrousel slides={[<Slide1 />, <Slide2 />, <Slide3 />]} />
       </div>
       <div className="relative">
         <BackButton to="/nosotros" />
