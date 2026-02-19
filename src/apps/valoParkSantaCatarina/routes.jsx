5d744d68884299c2ac0a548edcc6801c9ed3763e
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-19 02:00:14 -0600

    Layout para fondo, overlay e imagen de fondo

--- src/apps/valoParkSantaCatarina/routes.jsx
+++ src/apps/valoParkSantaCatarina/routes.jsx
@@ -5,15 +5,20 @@ import Ubicacion from "./pages/Ubicacion/Ubicacion";
 import Masterplan from "./pages/Masterplan/Masterplan";
 import Disponibilidad from "./pages/Disponibilidad/Disponibilidad";
 import Contacto from "./pages/Contacto/Contacto";
+import InformacionLayout from "./pages/Informacion/InformaciOnLayout";
 
 export default function ValoParkSantaCatarinaRoutes() {
   return (
     <Routes>
       <Route path="/valoPark/santaCatarina" element={<Home />} />
+
       <Route
         path="/valoPark/santaCatarina/informacion"
-        element={<Informacion />}
-      />
+        element={<InformacionLayout />}
+      >
+        <Route index element={<Informacion />} />
+      </Route>
+
       <Route path="/valoPark/santaCatarina/ubicacion" element={<Ubicacion />} />
       <Route
         path="/valoPark/santaCatarina/masterplan"
