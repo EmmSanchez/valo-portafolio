f0d16b2c4ce5b82ef42716569125619fe9a75f85
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-19 02:01:25 -0600

    Importación del carrusel

--- src/apps/valoParkSantaCatarina/pages/Informacion/Informacion.jsx
+++ src/apps/valoParkSantaCatarina/pages/Informacion/Informacion.jsx
@@ -1,5 +1,23 @@
+import BackButton from "@/components/shared/Buttons/BackButton";
+import { Carrousel } from "@/components/shared/Carrousel/Carrousel";
 import React from "react";
+import Slide1 from "../../components/Carrousel/Slides/Slide1";
+import Slide2 from "../../components/Carrousel/Slides/Slides2";
+import Slide3 from "../../components/Carrousel/Slides/Slide3";
 
 export default function Informacion() {
-  return <div className="w-dvw h-dvh bg-red-500">Informacion</div>;
+  return (
+    <div className="h-dvh w-dvw flex flex-col justify-end items-start overflow-hidden default-padding">
+      <div className="w-full flex flex-1">
+        <Carrousel slides={[<Slide1 />, <Slide2 />, <Slide3 />]} />
+      </div>
+
+      <div>
+        <BackButton
+          to="/valoPark/santaCatarina"
+          className="bg-santa-catarina hover:bg-santa-catarina-grey text-white"
+        />
+      </div>
+    </div>
+  );
 }
