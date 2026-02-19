87edf51291e0e995878c938ca0399f8ae7261e04
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-19 17:34:03 -0600

    Cambio en tamaño del logo

--- src/apps/valoParkSantaCatarina/pages/Informacion/InformacionLayout.jsx
+++ src/apps/valoParkSantaCatarina/pages/Informacion/InformacionLayout.jsx
@@ -6,7 +6,7 @@ export default function InformacionLayout() {
   return (
     <>
       <div className="absolute top-0 left-0 z-50 default-logo-padding">
-        <LogoValoParkSantaCatarina className="w-[clamp(50px,7.34vw,141px)] text-valo" />
+        <LogoValoParkSantaCatarina className="w-[clamp(58.33px,10.26vw,197px)] h-[clamp(17.76px,3.13vw,60px)]" />
       </div>
 
       <div className="absolute -z-50 w-dvw h-dvh">
