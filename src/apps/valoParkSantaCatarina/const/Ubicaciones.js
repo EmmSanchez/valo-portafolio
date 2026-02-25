da3a4566d44bf9214d7eba96968bb65e0c1eb241
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-24 22:31:33 -0600

    Datos de puntos de interes

---
+++ src/apps/valoParkSantaCatarina/const/Ubicaciones.js
@@ -0,0 +1,201 @@
+export const COORDENADAS = {
+  TRANSPORTE: [
+    {
+      id: "ruta_155",
+      label: "Ruta 155 Viñeros lomas inicios",
+      labelSide: "right",
+      coordinates: {
+        lat: 25.700563491361798,
+        lng: -100.50820677442198,
+      },
+    },
+    {
+      id: "avenida_las_torres",
+      label: "Avenida las Torres",
+      labelSide: "right",
+      coordinates: {
+        lat: 25.6960577640467,
+        lng: -100.4829139761495,
+      },
+    },
+    {
+      id: "parada_camion_sur",
+      label: "Parada de Camión",
+      labelSide: "right",
+      coordinates: {
+        lat: 25.68590818205809,
+        lng: -100.49026231902194,
+      },
+    },
+    {
+      id: "parada_camion_este",
+      label: "Parada de Camión",
+      labelSide: "right",
+      coordinates: {
+        lat: 25.68549050626129,
+        lng: -100.47668737310195,
+      },
+    },
+    {
+      id: "ruta_203",
+      label: "Ruta 203 Santa Catarina",
+      labelSide: "left",
+      coordinates: {
+        lat: 25.68285133929036,
+        lng: -100.48582023550867,
+      },
+    },
+  ],
+  CENTRO_COMERCIAL: [
+    {
+      id: "plaza_sendero",
+      label: "Plaza Sendero",
+      labelSide: "left",
+      coordinates: {
+        lat: 25.68825205544749,
+        lng: -100.49246010079166,
+      },
+    },
+    {
+      id: "plaza_catarinas",
+      label: "Plaza las Catarinas",
+      labelSide: "right",
+      coordinates: {
+        lat: 25.678778696510413,
+        lng: -100.48205507377597,
+      },
+    },
+    {
+      id: "plaza_cordilleras_virrey",
+      label: "Plaza Cordilleras del Virrey",
+      labelSide: "left",
+      coordinates: {
+        lat: 25.675600367932734,
+        lng: -100.48753039482946,
+      },
+    },
+  ],
+  CENTROS_RECREATIVOS: [
+    {
+      id: "radical_gym",
+      label: "Radical GYM",
+      labelSide: "left",
+      coordinates: {
+        lat: 25.689873484962085,
+        lng: -100.4970728993619,
+      },
+    },
+    {
+      id: "gym_basilio_navarro",
+      label: "GYM Basilio Navarro",
+      labelSide: "left",
+      coordinates: {
+        lat: 25.6813692579626,
+        lng: -100.4948501322945,
+      },
+    },
+    {
+      id: "gym_smart_fit_puerta",
+      label: "GYM Smart Fit la Puerta",
+      labelSide: "right",
+      coordinates: {
+        lat: 25.67960932143397,
+        lng: -100.48261576861935,
+      },
+    },
+  ],
+  SUPERMERCADOS: [
+    {
+      id: "bodega_los_vinedos",
+      label: "Bodega Aurrera Express, Los Viñedos",
+      labelSide: "left",
+      coordinates: {
+        lat: 25.704943112340125,
+        lng: -100.51664042768924,
+      },
+    },
+    {
+      id: "mini_super_rm",
+      label: "Mini Super RM",
+      labelSide: "right",
+      coordinates: {
+        lat: 25.701593868026418,
+        lng: -100.50866429799342,
+      },
+    },
+    {
+      id: "bodega_hacienda_huasteca",
+      label: "Bodega Aurrera Hacienda la Huasteca",
+      labelSide: "right",
+      coordinates: {
+        lat: 25.697620415553622,
+        lng: -100.49866693451716,
+      },
+    },
+    {
+      id: "mini_super_coronado",
+      label: "Mini Super coronado",
+      labelSide: "right",
+      coordinates: {
+        lat: 25.69562811751869,
+        lng: -100.4957776877314,
+      },
+    },
+    {
+      id: "heb_la_puerta",
+      label: "HEB La Puerta",
+      labelSide: "left",
+      coordinates: {
+        lat: 25.684401684249803,
+        lng: -100.48655627462264,
+      },
+    },
+    {
+      id: "soriana_la_puerta",
+      label: "Soriana Hiper La Puerta",
+      labelSide: "right",
+      coordinates: {
+        lat: 25.683241425751106,
+        lng: -100.48389552332021,
+      },
+    },
+  ],
+  CENTRO_DE_SALUD: [
+    {
+      id: "centro_medico_santa_catarina",
+      label: "Centro Médico Santa Catarina",
+      labelSide: "left",
+      coordinates: {
+        lat: 25.695733305904962,
+        lng: -100.5003298394517,
+      },
+    },
+    {
+      id: "hospital_general_regional",
+      label: "Hospital General Regional",
+      labelSide: "left",
+      coordinates: {
+        lat: 25.68838542617405,
+        lng: -100.51257558976747,
+      },
+    },
+    {
+      id: "clinica_64_santa_catarina",
+      label: "Clínica 64 Santa Catarina Nuevo León",
+      labelSide: "right",
+      coordinates: {
+        lat: 25.682893993312184,
+        lng: -100.48648690345858,
+      },
+    },
+    {
+      id: "hospital_humilde",
+      label: "Hospital Humilde SA DE CV",
+      labelSide: "right",
+      coordinates: {
+        lat: 25.67494036411761,
+        lng: -100.48033005542433,
+      },
+    },
+  ],
+};
