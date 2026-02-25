37db4ed99cf79993bcef5096877ace00c1c00d80
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-24 22:33:12 -0600

    Renderizar Markers dependiendo de valor de subfilter

--- src/components/shared/Map/GoogleMaps.jsx
+++ src/components/shared/Map/GoogleMaps.jsx
@@ -1,10 +1,11 @@
 import { APIProvider, Map } from "@vis.gl/react-google-maps";
-import { useEffect } from "react";
-import { useMap } from "@vis.gl/react-google-maps";
 import CustomMarker from "./CustomMarker";
 import VPSCLandmarkIcon from "@/apps/valoPortafolio/assets/icons/proyectos/markers/ValoParkSantaCatarinaLandmark.svg";
-import { AdvancedMarker } from "@vis.gl/react-google-maps";
-import busLandmark from "@/apps/valoParkSantaCatarina/assets/icons/ubicacion/puntos_interes/busLandmark.svg";
+import TransporteMarkers from "@/apps/valoParkSantaCatarina/components/Ubicaciones/puntos-interes/TransporteMarkers";
+import CentroComercialMarkers from "@/apps/valoParkSantaCatarina/components/Ubicaciones/puntos-interes/CentroComercialMarkers";
+import CentrosRecreativosMarkers from "@/apps/valoParkSantaCatarina/components/Ubicaciones/puntos-interes/CentrosRecreativosMarkers";
+import SupermercadosMarkers from "@/apps/valoParkSantaCatarina/components/Ubicaciones/puntos-interes/SupermercadosMarkers";
+import CentrosSaludMarkers from "@/apps/valoParkSantaCatarina/components/Ubicaciones/puntos-interes/CentrosSaludMarkers";
 const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
 const ValoParkSantaCatarinaCoordinates = {
@@ -17,6 +18,7 @@ const initialCameraCenter = {
   lng: -100.48971351880338,
 };
 const initialZoom = 14;
+const initialHeading = 9;
 
 const SUBFILTER = {
   TRANSPORTE_PUBLICO: "transporte",
@@ -26,65 +28,17 @@ const SUBFILTER = {
   CENTRO_DE_SALUD: "centro-salud",
 };
 
-const COORDENADAS = {
-  TRANSPORTE: [
-    {
-      id: "ruta_155",
-      label: "Ruta 155 Viñeros lomas inicios",
-      coordinates: {
-        lat: 25.700563491361798,
-        lng: -100.50820677442198,
-      },
-    },
-    {
-      id: "avenida_las_torres",
-      label: "Avenida las Torres",
-      coordinates: {
-        lat: 25.6960577640467,
-        lng: -100.4829139761495,
-      },
-    },
-    {
-      id: "parada_camion_sur",
-      label: "Parada de Camión",
-      coordinates: {
-        lat: 25.68590818205809,
-        lng: -100.49026231902194,
-      },
-    },
-    {
-      id: "parada_camion_este",
-      label: "Parada de Camión",
-      coordinates: {
-        lat: 25.68549050626129,
-        lng: -100.47668737310195,
-      },
-    },
-    {
-      id: "ruta_203",
-      label: "Ruta 203 Santa Catarina",
-      coordinates: {
-        lat: 25.68285133929036,
-        lng: -100.48582023550867,
-      },
-    },
-  ],
-  CENTRO_COMERCIAL: {},
-  CENTROS_RECREATIVOS: {},
-  SUPERMERCADOS: {},
-  CENTRO_DE_SALUD: {},
-};
-
 export default function GoogleMaps({ filter = "" }) {
   return (
     <APIProvider apiKey={API_KEY}>
       <Map
         mapId="82766ca9d435febc2711df52"
+        mapTypeId="satellite"
+        disableDefaultUI
         defaultZoom={initialZoom}
         defaultCenter={initialCameraCenter}
-        mapTypeId="satellite"
+        heading={initialHeading}
         zoomControl={false}
-        disableDefaultUI
         fullscreenControl={false}
         streetViewControl={false}
         mapTypeControl={false}
@@ -104,66 +58,13 @@ export default function GoogleMaps({ filter = "" }) {
           size={80}
         />
         {filter === SUBFILTER.TRANSPORTE_PUBLICO && <TransporteMarkers />}
+        {filter === SUBFILTER.CENTRO_COMERCIAL && <CentroComercialMarkers />}
+        {filter === SUBFILTER.CENTROS_RECREATIVOS && (
+          <CentrosRecreativosMarkers />
+        )}
+        {filter === SUBFILTER.SUPERMERCADOS && <SupermercadosMarkers />}
+        {filter === SUBFILTER.CENTRO_DE_SALUD && <CentrosSaludMarkers />}
       </Map>
     </APIProvider>
-  );
-}
-
-function MapDebugger() {
-  const map = useMap();
-
-  useEffect(() => {
-    if (!map) return;
-
-    const handleIdle = () => {
-      const center = map.getCenter();
-      const zoom = map.getZoom();
-
-      console.log("Center:", {
-        lat: center.lat(),
-        lng: center.lng(),
-      });
-
-      console.log("Zoom:", zoom);
-    };
-
-    map.addListener("idle", handleIdle);
-
-    return () => {
-      window.google.maps.event.clearListeners(map, "idle");
-    };
-  }, [map]);
-
-  return null;
-}
-
-function TransporteMarkers() {
-  return (
-    <>
-      {COORDENADAS.TRANSPORTE.map((transporte) => {
-        return (
-          <AdvanceMarker key={transporte.id} position={transporte.coordinates}>
-            <div className="relative flex">
-              <img
-                src={busLandmark}
-                alt="Bus Landmark"
-                style={{ width: 36, height: 40 }}
-              />
-              <div className="absolute left-5 top-1 -z-10 w-fit py-[clamp(1.48px,0.26vw,5px)] pr-[clamp(2.96px,0.52vw,10px)] pl-[clamp(7.4px,1.3vw,25px)] whitespace-nowrap bg-santa-catarina font-semibold uppercase">
-                {transporte.label}
-              </div>
-            </div>
-          </AdvanceMarker>
-        );
-      })}
-    </>
-  );
-}
-
-function AdvanceMarker({ size, position, children }) {
-  return (
-    <AdvancedMarker position={position}>
-      <div style={{ width: size, height: size }}>{children}</div>
-    </AdvancedMarker>
   );
 }
