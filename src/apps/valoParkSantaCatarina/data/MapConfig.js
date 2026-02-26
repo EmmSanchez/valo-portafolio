9fdc54852049685b25ba5449006e5970666c6429
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-25 19:58:15 -0600

    Configuraciones guardadas

---
+++ src/apps/valoParkSantaCatarina/data/MapConfig.js
@@ -0,0 +1,11 @@
+export const MAP_CONFIG = {
+  id: "82766ca9d435febc2711df52",
+  zoom: 14,
+  heading: 9,
+  center: { lat: 25.701990146668596, lng: -100.48971351880338 },
+};
+
+export const VPSC_LANDMARK_POSITION = {
+  lat: 25.689151218948044,
+  lng: -100.48962768811491,
+};
