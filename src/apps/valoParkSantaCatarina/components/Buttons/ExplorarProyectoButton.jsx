4c95c9dc5174b172efc960f7a4341412f7c1c60e
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-26 01:17:13 -0600

    Redirigir a Masterplan

--- src/apps/valoParkSantaCatarina/components/Buttons/ExplorarProyectoButton.jsx
+++ src/apps/valoParkSantaCatarina/components/Buttons/ExplorarProyectoButton.jsx
@@ -1,14 +1,18 @@
 import ClickIcon from "../../assets/icons/ubicacion/ClickIcon";
+import { Link } from "react-router";
 
 export default function ExplorarProyectoButton() {
   return (
     <div className="absolute w-dvw h-dvh flex items-center justify-center">
-      <button className="relative z-10 flex items-center justify-between px-[clamp(13.32px,2.34vw,45px)] py-[clamp(6.81px,1.2vw,23px)] gap-[clamp(7.4px,1.3vw,25px)] bg-santa-catarina hover:cursor-pointer">
+      <Link
+        to={"/valoPark/santaCatarina/masterplan"}
+        className="relative z-10 flex items-center justify-between px-[clamp(13.32px,2.34vw,45px)] py-[clamp(6.81px,1.2vw,23px)] gap-[clamp(7.4px,1.3vw,25px)] bg-santa-catarina hover:cursor-pointer"
+      >
         <p className="text-paragraph-button font-semibold whitespace-nowrap">
           Explorar Proyecto
         </p>
         <ClickIcon className="shrink-0 w-[clamp(7.11px,1.25vw,24.005px)] h-[clamp(8.01px,1.41vw,27.054px)]" />
-      </button>
+      </Link>
 
       {/* Blur superior */}
       <div
