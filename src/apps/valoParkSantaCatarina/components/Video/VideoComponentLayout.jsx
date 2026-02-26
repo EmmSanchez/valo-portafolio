b0e6b89a5e1de16e450a969ea4552570f793b522
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-26 11:35:04 -0600

    Mapeo de videos desde json portadas y rde, leer parametro "position"

--- src/apps/valoParkSantaCatarina/components/Video/VideoComponentLayout.jsx
+++ src/apps/valoParkSantaCatarina/components/Video/VideoComponentLayout.jsx
@@ -1,20 +1,29 @@
-import { Outlet } from "react-router";
-import videos from "../../data/videos.json";
+import { Outlet, useSearchParams } from "react-router";
 import { useLocation } from "react-router";
 import { useRef, useEffect } from "react";
+import portadas from "../../data/Videos/portadas.json";
+import rde from "../../data/Videos/rde.json";
 
 const videoMap = {
-  "/valoPark/santaCatarina": videos.videos.portada.idle,
-  "/valoPark/santaCatarina/informacion": videos.videos.portada.idle,
-  "/valoPark/santaCatarina/masterplan": videos.videos.masterplan.idle,
+  "/valoPark/santaCatarina": portadas.informacion,
+  "/valoPark/santaCatarina/informacion": portadas.informacion,
+  "/valoPark/santaCatarina/masterplan?position=1": rde.videos[0].src,
+  "/valoPark/santaCatarina/masterplan?position=2": rde.videos[2].src,
+  "/valoPark/santaCatarina/masterplan?position=3": rde.videos[4].src,
+  "/valoPark/santaCatarina/masterplan?position=4": rde.videos[6].src,
+  "/valoPark/santaCatarina/masterplan?position=5": rde.videos[8].src,
 };
 
 export default function VideoComponentLayout() {
   const videoRef = useRef();
   const prevSrcRef = useRef(null);
-  const { pathname } = useLocation();
 
-  const videoSrc = videoMap[pathname];
+  const [searchParams] = useSearchParams();
+  const { pathname } = useLocation();
+  const position = searchParams.get("position");
+
+  const videoKey = position ? `${pathname}?position=${position}` : pathname;
+  const videoSrc = videoMap[videoKey];
 
   useEffect(() => {
     if (!videoSrc || prevSrcRef.current === videoSrc) return;
@@ -23,6 +32,7 @@ export default function VideoComponentLayout() {
     videoRef.current.src = videoSrc;
     videoRef.current.play();
   }, [videoSrc]);
+
   return (
     <>
       <div className="absolute -z-50">
@@ -32,7 +42,7 @@ export default function VideoComponentLayout() {
           loop
           muted
           playsInline
-          className="w-dvw h-dvh"
+          className="w-dvw h-dvh object-cover"
           src={videoSrc}
         ></video>
       </div>
