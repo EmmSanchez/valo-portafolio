650d81166c4703c792e8ab8ad2eb8a4219165824
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-25 19:24:59 -0600

    Cambios en fuente y padding

--- src/apps/valoParkSantaCatarina/components/Ubicaciones/puntos-interes/CentrosSaludMarkers.jsx
+++ src/apps/valoParkSantaCatarina/components/Ubicaciones/puntos-interes/CentrosSaludMarkers.jsx
@@ -17,7 +17,9 @@ export default function CentrosSaludMarkers() {
               <div
                 className={`absolute top-1/2 -translate-y-1/2 -z-10 w-fit py-[clamp(1.48px,0.26vw,5px)] whitespace-nowrap bg-santa-catarina font-semibold uppercase ${item.labelSide === "left" ? "right-1/2 pl-[clamp(2.96px,0.52vw,10px)] pr-[clamp(7.4px,1.3vw,25px)]" : "left-1/2 pr-[clamp(2.96px,0.52vw,10px)] pl-[clamp(7.4px,1.3vw,25px)]"}`}
               >
-                <p className="text-puntos-interes py-px">{item.label}</p>
+                <p className="text-puntos-interes pt-1 2xl:pt-1.5 font-eurostile">
+                  {item.label}
+                </p>
               </div>
             </div>
           </AdvanceMarker>
