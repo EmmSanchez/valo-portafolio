371fac693373f41c031e7e569e550a756ebd89d4
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-26 01:18:33 -0600

    Botón regresar

--- src/apps/valoParkSantaCatarina/pages/Masterplan/Masterplan.jsx
+++ src/apps/valoParkSantaCatarina/pages/Masterplan/Masterplan.jsx
@@ -1,3 +1,12 @@
+import BackButton from "@/components/shared/Buttons/BackButton";
+
 export default function Masterplan() {
-  return <div className="w-dvw h-dvh bg-green-500">Masterplan</div>;
+  return (
+    <div className="w-dvw h-dvh flex default-padding">
+      <BackButton
+        to="/valoPark/santaCatarina"
+        className="self-end bg-santa-catarina hover:bg-santa-catarina-grey"
+      />
+    </div>
+  );
 }
