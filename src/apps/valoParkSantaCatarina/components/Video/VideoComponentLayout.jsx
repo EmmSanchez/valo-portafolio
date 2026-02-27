6d85f7acf864fd13120a5c071e42ae5416a85d1f
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-26 22:20:38 -0600

    Componente para renderizar videos con dos buffers, invocando usePlayerVideo y escuchando la url

--- src/apps/valoParkSantaCatarina/components/Video/VideoComponentLayout.jsx
+++ src/apps/valoParkSantaCatarina/components/Video/VideoComponentLayout.jsx
@@ -3,50 +3,53 @@ import { useLocation } from "react-router";
 import { useRef, useEffect } from "react";
 import portadas from "../../data/Videos/portadas.json";
 import rde from "../../data/Videos/rde.json";
+import { useVideoPlayer } from "../../hooks/useVideoPlayer";
+import { PLAYER } from "../../const/Videos";
+import { useContext } from "react";
+import { VideoPlayerContext } from "../../context/VideoPlayerContext";
 
 const videoMap = {
   "/valoPark/santaCatarina": portadas.informacion,
   "/valoPark/santaCatarina/informacion": portadas.informacion,
-  "/valoPark/santaCatarina/masterplan?position=1": rde.videos[0].src,
-  "/valoPark/santaCatarina/masterplan?position=2": rde.videos[2].src,
-  "/valoPark/santaCatarina/masterplan?position=3": rde.videos[4].src,
-  "/valoPark/santaCatarina/masterplan?position=4": rde.videos[6].src,
-  "/valoPark/santaCatarina/masterplan?position=5": rde.videos[8].src,
 };
 
 export default function VideoComponentLayout() {
-  const videoRef = useRef();
-  const prevSrcRef = useRef(null);
-
+  const { setIdle, setTransitioning } = useContext(VideoPlayerContext);
   const [searchParams] = useSearchParams();
-  const { pathname } = useLocation();
   const position = searchParams.get("position");
 
-  const videoKey = position ? `${pathname}?position=${position}` : pathname;
-  const videoSrc = videoMap[videoKey];
+  const { videoRefA, videoRefB, goTo, activePlayer } = useVideoPlayer({
+    json: rde,
+  });
 
   useEffect(() => {
-    if (!videoSrc || prevSrcRef.current === videoSrc) return;
+    if (position) {
+      goTo(Number(position));
+    }
+  }, [position]);
 
-    prevSrcRef.current = videoSrc;
-    videoRef.current.src = videoSrc;
-    videoRef.current.play();
-  }, [videoSrc]);
+  useEffect(() => {
+    if (activePlayer === PLAYER.A) return setIdle();
+    if (activePlayer === PLAYER.B) return setTransitioning();
+  }, [activePlayer]);
 
   return (
     <>
-      <div className="absolute -z-50">
-        <video
-          ref={videoRef}
-          autoPlay
-          loop
-          muted
-          playsInline
-          className="w-dvw h-dvh object-cover"
-          src={videoSrc}
-        ></video>
-      </div>
+      <video
+        ref={videoRefA}
+        autoPlay
+        muted
+        playsInline
+        className={`absolute inset-0 -z-50 w-dvw h-dvh object-cover ${activePlayer === PLAYER.A ? "opacity-100" : "opacity-0"}`}
+      ></video>
 
+      <video
+        ref={videoRefB}
+        autoPlay
+        muted
+        playsInline
+        className={`absolute inset-0 -z-50 w-dvw h-dvh object-cover ${activePlayer === PLAYER.B ? "opacity-100" : "opacity-0"}`}
+      ></video>
       <Outlet />
     </>
   );
