cb8545c38b77ec1a1b356f531a1bc644e74496f0
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-27 18:31:07 -0600

    Guardar en una referencia las funciones

--- src/apps/valoParkSantaCatarina/context/VideoPlayerProvider.jsx
+++ src/apps/valoParkSantaCatarina/context/VideoPlayerProvider.jsx
@@ -1,17 +1,12 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { VideoPlayerContext } from "./VideoPlayerContext";
 import { MODE } from "../const/Videos";
 
 export function VideoPlayerProvider({ children }) {
   const [mode, setMode] = useState(MODE.IDLE);
 
-  const setTransitioning = () => {
-    setMode(MODE.TRANSITIONING);
-  };
-
-  const setIdle = () => {
-    setMode(MODE.IDLE);
-  };
+  const setTransitioning = useCallback(() => setMode(MODE.TRANSITIONING), []);
+  const setIdle = useCallback(() => setMode(MODE.IDLE), []);
 
   return (
     <VideoPlayerContext.Provider value={{ mode, setIdle, setTransitioning }}>
