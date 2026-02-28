cf8615b9d00f13ce6af28ee50ea93d2842680e3d
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-02-28 15:10:36 -0600

    Archivo con los datos de los botones del submenu de masterplan

---
+++ src/apps/valoParkSantaCatarina/data/SubmenuMasterplan.js
@@ -0,0 +1,16 @@
+export const SUBMENU_MASTERPLAN = {
+  id: "masterplan-vpsc",
+  label: "Masterplan",
+  buttons: [
+    {
+      id: "ventajas-vpsc",
+      label: "Ventajas de proyecto",
+      filter: "ventajas",
+    },
+    {
+      id: "naves-vpsc",
+      label: "Naves industriales",
+      filter: "naves-industriales",
+    },
+  ],
+};
