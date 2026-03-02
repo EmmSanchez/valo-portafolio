eae832c0dde88e40dff5ff019946da34aa81d7f9
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-03-02 04:55:22 -0600

    Uso de json para vti ventajas, detectar por url parametro ventaja, callback para manejar modo automatico

--- src/apps/valoParkSantaCatarina/components/Video/VideoComponentLayout.jsx
+++ src/apps/valoParkSantaCatarina/components/Video/VideoComponentLayout.jsx
@@ -1,32 +1,76 @@
-import { Outlet, useSearchParams } from "react-router";
-import { useLocation } from "react-router";
-import { useRef, useEffect } from "react";
-import portadas from "../../data/Videos/portadas.json";
-import rde from "../../data/Videos/rde.json";
-import { useVideoPlayer } from "../../hooks/useVideoPlayer";
-import { PLAYER, MODE } from "../../const/Videos";
+import { useEffect } from "react";
 import { useContext } from "react";
+import { useVideoPlayer } from "../../hooks/useVideoPlayer";
+import { Outlet, useSearchParams } from "react-router";
+// import { useLocation } from "react-router";
+// import portadas from "../../data/Videos/portadas.json";
+import { PLAYER, MODE } from "../../const/Videos";
 import { VideoPlayerContext } from "../../context/VideoPlayerContext";
+import rde from "../../data/Videos/rde.json";
+import vti_ventajas from "../../data/Videos/vti_ventajas.json";
+import { useLocation } from "react-router";
 
-const videoMap = {
-  "/valoPark/santaCatarina": portadas.informacion,
-  "/valoPark/santaCatarina/informacion": portadas.informacion,
+// const videoMap = {
+//   "/valoPark/santaCatarina": portadas.informacion,
+//   "/valoPark/santaCatarina/informacion": portadas.informacion,
+// };
+
+const VENTAJA_TO_POSITION = {
+  "ubicacion-privilegiada": 1,
+  "cercania-centro-comercial": 2,
+  "porton-controlado": 3,
+  "espacio-industrial-clase-a": 4,
+  "patio-de-maniobras": 5,
+  "bodegas-listas": 6,
+};
+const POSITION_TO_VENTAJA = {
+  1: "ubicacion-privilegiada",
+  2: "cercania-centro-comercial",
+  3: "porton-controlado",
+  4: "espacio-industrial-clase-a",
+  5: "patio-de-maniobras",
+  6: "bodegas-listas",
+};
+
+const VIDEOS_MAP = {
+  "/valoPark/santaCatarina/masterplan": rde,
+  "/valoPark/santaCatarina/masterplan/ventajas-de-proyecto/video-tour":
+    vti_ventajas,
 };
 
 export default function VideoComponentLayout() {
   const { setIdle, setTransitioning } = useContext(VideoPlayerContext);
-  const [searchParams] = useSearchParams();
+  const { pathname } = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
   const position = searchParams.get("position");
+  const ventaja = searchParams.get("ventaja");
+
+  const videosRunning = VIDEOS_MAP[pathname];
 
   const { videoRefA, videoRefB, goTo, activePlayer, modeState } =
     useVideoPlayer({
-      json: rde,
+      json: videosRunning ?? rde,
+      onPositionChange: (pos) => {
+        if (pathname.includes("video-tour")) {
+          setSearchParams({ ventaja: POSITION_TO_VENTAJA[pos] });
+        } else {
+          setSearchParams({ position: pos });
+        }
+      },
     });
 
   useEffect(() => {
     if (!position) return;
     goTo(Number(position));
   }, [position]);
+
+  // PARA VTI VENTAJAS
+  useEffect(() => {
+    if (!ventaja) return;
+    const mappedPosition = VENTAJA_TO_POSITION[ventaja];
+    if (!mappedPosition) return;
+    goTo(mappedPosition);
+  }, [ventaja]);
 
   useEffect(() => {
     if (modeState === MODE.IDLE) return setIdle();
