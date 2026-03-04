4d12a95a37bcf6ca220cefdf3f647d30807e9315
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2026-03-04 16:33:00 -0600

    Quitar flechas

--- src/apps/valoParkSantaCatarina/components/Masterplan/Ventajas/Recorrido360.jsx
+++ src/apps/valoParkSantaCatarina/components/Masterplan/Ventajas/Recorrido360.jsx
@@ -6,7 +6,7 @@ export default function Recorrido360() {
     <div className="flex w-dvw h-dvh default-padding pointer-events-none">
       <KuulaComponent
         src={
-          "https://kuula.co/share/collection/7DZBj?logo=-1&info=0&fs=0&vr=0&zoom=1&initload=1&thumbs=-&1margin=30&keys=0"
+          "https://kuula.co/share/collection/7DZBj?logo=-1&info=0&fs=0&vr=0&zoom=1&initload=1&thumbs=-1&margin=30&alpha=0.81&inst=0&keys=0"
         }
       />
 
